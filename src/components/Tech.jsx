24e94ac34b1d03928b05ee6135acd38e43ef3922
Ema0502 <ema050296@gmail.com>
2024-03-26 10:34:00 -0300

    errors resolved

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,25 +3,16 @@ import React from "react";
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
-import { motion } from "framer-motion";
-import { textVariant } from "../utils/motion";
-import { styles } from "../styles";
 
 const Tech = () => {
   return (
-    <>
-      <motion.div variants={textVariant()} className="pb-12">
-        <p className={styles.sectionSubText}>HERRAMIENTAS UTILIZADAS</p>
-        <h2 className={styles.sectionHeadText}>Tecnologías</h2>
-      </motion.div>
-      <div className="flex flex-row flex-wrap justify-center gap-10">
-        {technologies.map((technology) => (
-          <div className="w-28 h-28" key={technology.name}>
-            <BallCanvas icon={technology.icon} />
-          </div>
-        ))}
-      </div>
-    </>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   );
 };
 
